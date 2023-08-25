b37b23d2536a20a10d67a40c5a2fdedcb0d40b49
himanshu <himanshu@algodommedia.com>
2023-08-25 16:06:16 +0530

    update client version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='newsdataapi',
-    version='0.1.7',
+    version='0.1.8',
     packages=['newsdataapi'],
     description='Python library for newsdata client-API Call',
     long_description=long_description,
