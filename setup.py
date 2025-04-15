86bff7f1aac413ac75363e387bdc279ce94f5e15
himanshu <himanshu@bytesview.com>
2025-04-15 13:36:11 +0530

    added option to get response headers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='newsdataapi',
-    version='0.1.21',
+    version='0.1.22',
     packages=['newsdataapi'],
     description='Python library for newsdata client-API Call',
     long_description=long_description,
